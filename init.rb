a816b43686caa08a9206dad55456f767e4279aa8
Alex Sinelnikov <sinelniko@gmail.com>
2015-02-03 14:03:33 +0200

    fixed wrong class name

--- init.rb
+++ init.rb
@@ -10,10 +10,9 @@ Redmine::Plugin.register :redmine_wiki_fast_history do
   author_url 'http://example.com/about'
 end
 
-
-class FilesHook < Redmine::Hook::ViewListener
+class AssetViewsLayoutsHook < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context = { })
-      javascript_include_tag('ZeroClipboard.js', :plugin => 'redmine_wiki_fast_history')+
+    javascript_include_tag('ZeroClipboard.js', :plugin => 'redmine_wiki_fast_history')+
         javascript_include_tag('jquery.timeago.js', :plugin => 'redmine_wiki_fast_history')
   end
 end
